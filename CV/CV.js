868f560a32d3b7405f10f6eafd04ad528ebaae43
fj3ojitas <126539157+fj3ojitas@users.noreply.github.com>
2023-03-30 19:19:00 -0300

    Cvcargado

---
+++ CV/CV.js
@@ -0,0 +1,25 @@
+
+
+document.getElementById('boton1').onclick = function(){
+
+   document.getElementById("domicilio").innerHTML = "Domicilio: Avenida callefalsa 123";
+
+}
+
+document.getElementById('boton2').onclick = function(){
+
+   document.getElementById("telefono").innerHTML = "Telefono: 119555878";
+
+}
+
+document.getElementById('boton3').onclick = function(){
+
+   document.getElementById("email").innerHTML = "Email: jeancarlitos@hotmai.com";
+
+}
+
+document.getElementById('boton4').onclick = function(){
+
+   document.getElementById("estadocivil").innerHTML = "Estado civil: Soltero";
+
+}
